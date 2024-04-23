9c27aac7d1c3f69bd2a65b5c8bb5495c79039d73
Simon Théorêt <simonteoret@hotmail.com>
2024-04-23 16:29:25 -0400

    magit is A-OK

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from arch.mobilevit import MobileViT
 from architecture import GenericTrainer
 from exp_datasets import AdversarialCifar10, GaussianCifar10, VanillaCifar10
 
-torch.manual_seed(42)  # need to iterate over seeds
+torch.manual_seed(42)
 
 
 def main_generic(
