1a3e703e4111913d716f1477ac3245987c057c40
Simon Théorêt <simonteoret@hotmail.com>
2024-04-24 23:39:45 -0400

    few small corrections + commented out something in runner.sh

--- runner.sh
+++ runner.sh
@@ -8,8 +8,8 @@
 # adamw
 python3 -m main --exp_name=allcnnvanillaadamw --model_name=allcnn --optim_name=adamw --dataset_name=vanilla
 python3 -m main --exp_name=allcnngaussianadamw --model_name=allcnn --optim_name=adamw --dataset_name=gaussian
-# python3 -m main --exp_name=allcnnadvadamw --model_name=allcnn --optim_name=adamw --dataset_name=adv
-#sgd
+# # python3 -m main --exp_name=allcnnadvadamw --model_name=allcnn --optim_name=adamw --dataset_name=adv
+# #sgd
 python3 -m main --exp_name=allcnnvanillasgd --model_name=allcnn --optim_name=sgd --dataset_name=vanilla
 python3 -m main --exp_name=allcnngaussiansgd --model_name=allcnn --optim_name=sgd --dataset_name=gaussian
 # python3 -m main --exp_name=allcnnadvsgd --model_name=allcnn --optim_name=sgd --dataset_name=adv
@@ -24,4 +24,3 @@ python3 -m main --exp_name=mobilevitadamwgaussian --model_name=mobilevit --optim
 python3 -m main --exp_name=mobilevitsgdvanilla --model_name=mobilevit --optim_name=sgd --dataset_name=vanilla
 python3 -m main --exp_name=mobilevitsgdgaussian --model_name=mobilevit --optim_name=sgd --dataset_name=gaussian
 # python3 -m main --exp_name=mobilevitsgdadv --model_name=mobilevit --optim_name=sgd --dataset_name=adv
-
